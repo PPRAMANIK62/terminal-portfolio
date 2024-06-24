10f9eac9322f7b06bc6e67741bc5a9fffb3558d5
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-24 10:15:32 +0530

    about command done

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -1,5 +1,25 @@
+import {
+  AboutWrapper,
+  HighlightAlt,
+  HighlightSpan,
+} from "../styles/About.styled";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <AboutWrapper data-testid="about">
+      <p>
+        Hi, my name is <HighlightSpan>Purbayan Pramanik</HighlightSpan>!
+      </p>
+      <p>
+        I&apos;m <HighlightAlt>a full-stack developer</HighlightAlt> based in
+        Kolkata, India
+      </p>
+      <p>
+        I am passionate about writing code and <br />
+        developing web applications to solve real-life challenges.
+      </p>
+    </AboutWrapper>
+  );
 };
 
 export default About;
