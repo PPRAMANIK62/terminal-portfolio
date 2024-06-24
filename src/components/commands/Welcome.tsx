e34662e664b8d98fa860ab45c3076e04fad92cf7
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-24 15:38:46 +0530

    welcome command done

--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -1,5 +1,87 @@
+import { Cmd } from "../styles/Help.styled";
+import {
+  HeroContainer,
+  Link,
+  PreImg,
+  PreName,
+  PreNameMobile,
+  PreWrapper,
+  Seperator,
+} from "../styles/Welcome.styled";
+
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <HeroContainer data-testid="welcome">
+      <div className="info-section">
+        <PreName>
+          {`
+██████╗ ██╗   ██╗██████╗ ██████╗  █████╗ ██╗   ██╗ █████╗ ███╗   ██╗
+██╔══██╗██║   ██║██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝██╔══██╗████╗  ██║
+██████╔╝██║   ██║██████╔╝██████╔╝███████║ ╚████╔╝ ███████║██╔██╗ ██║
+██╔═══╝ ██║   ██║██╔══██╗██╔══██╗██╔══██║  ╚██╔╝  ██╔══██║██║╚██╗██║
+██║     ╚██████╔╝██║  ██║██████╔╝██║  ██║   ██║   ██║  ██║██║ ╚████║
+╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                                    
+██████╗ ██████╗  █████╗ ███╗   ███╗ █████╗ ███╗   ██╗██╗██╗  ██╗    
+██╔══██╗██╔══██╗██╔══██╗████╗ ████║██╔══██╗████╗  ██║██║██║ ██╔╝    
+██████╔╝██████╔╝███████║██╔████╔██║███████║██╔██╗ ██║██║█████╔╝     
+██╔═══╝ ██╔══██╗██╔══██║██║╚██╔╝██║██╔══██║██║╚██╗██║██║██╔═██╗     
+██║     ██║  ██║██║  ██║██║ ╚═╝ ██║██║  ██║██║ ╚████║██║██║  ██╗    
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝    `}
+        </PreName>
+        <PreWrapper>
+          <PreNameMobile>
+            {`
+╔═╗┬ ┬┬─┐┌┐ ┌─┐┬ ┬┌─┐┌┐┌  
+╠═╝│ │├┬┘├┴┐├─┤└┬┘├─┤│││  
+╩  └─┘┴└─└─┘┴ ┴ ┴ ┴ ┴┘└┘  
+╔═╗┬─┐┌─┐┌┬┐┌─┐┌┐┌┬┬┌─    
+╠═╝├┬┘├─┤│││├─┤││││├┴┐    
+╩  ┴└─┴ ┴┴ ┴┴ ┴┘└┘┴┴ ┴    `}
+          </PreNameMobile>
+        </PreWrapper>
+        <div>Welcome to my terminal portfolio. (Version 1.0.0)</div>
+        <Seperator>----</Seperator>
+        <div>
+          This project's source code can be found in this project's{" "}
+          <Link
+            href="https://github.com/PPRAMANIK62/terminal-portfolio"
+            target="_blank"
+          >
+            GitHub repo
+          </Link>
+          .
+        </div>
+        <Seperator>----</Seperator>
+        <div>
+          For a list of available commands, type `<Cmd>help</Cmd>`.
+        </div>
+      </div>
+      <div className="illu-section">
+        <PreImg>
+          {`
+                   ,##,,eew,
+                 ,##############C
+              a###############@##
+             7####^\`^"7W7^"@####
+             @#@b\`         ^@#@^
+              ##^,,,,   ,,,,^#^
+             ,,@######"#######=
+              .''555"\` '5555b|
+              T"@  ,,,^,mg,@,*
+                 %p||\`~~'.#\`
+                  ^Wp  ,#T
+                 :b''@@b^}
+              ,^     \` 'b 3-
+          .<\` 'p   ^v   #   b   *.
+        {      }   #"GpGb   [
+        C      3 * @#######Nl      \`
+       '            ^@##b     ($    !
+     `}
+        </PreImg>
+      </div>
+    </HeroContainer>
+  );
 };
 
 export default Welcome;
